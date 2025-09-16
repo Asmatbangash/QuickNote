784779b2df280764d18c4d4cf67d6464bf901808
Asmat Ullah <bangashasmat877@gmail.com>
2025-09-16 10:55:56 +0500

    modify index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -11,13 +11,7 @@ const port = process.env.PORT || 3000;
 dbConnection();
 
 // middle ware
-app.use(
-  cors({
-    origin: ["https://quick-note-sx55.vercel.app"],
-    methods: ["GET", "POST", "DELETE", "UPDATE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1/noteapp/", router);
 
